bf38a68cdacb83235b9bdee2a1aaa0a631c49dfb
Gabriel Marcucci <gamarcucci@gmail.com>
2023-01-02 21:32:17 -0300

    Commit

---
+++ Estudo/Introducao/script.js
@@ -0,0 +1,22 @@
+//alert('Seja bem vindo!')
+
+document.getElementById('nome').value = 'teste'
+
+var texto = 'Javascript'
+var nome = prompt('Informe o seu nome')
+
+var numeroInt = -6
+var numeroFra = 11.3489 
+
+var condicionais = false
+
+// if (condicionais) {
+//    alert('true') 
+// }else{
+//     alert('false')
+// }
+
+document.write('<h3>Olá eu sou o ' + nome + ', tudo bem?</h3>')
+
+
+
