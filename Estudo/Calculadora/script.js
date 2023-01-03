0191317656f67f1b6a8b2b0b47e2288f5cba0130
Gabriel Marcucci <gamarcucci@gmail.com>
2023-01-03 20:34:13 -0300

    Commit

---
+++ Estudo/Calculadora/script.js
@@ -0,0 +1,12 @@
+
+var a = parseInt(prompt())
+
+for (let index = 1; index <= 50; index++) {
+    
+    var b = index    
+    
+    var resultado = (a * b) 
+
+    document.write(a + ' X ' + b + ' = ' + resultado + '<br/>')
+
+}
