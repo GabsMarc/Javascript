24c4a23ad6574d536797e0e30861cd6f83efe877
Gabriel Marcucci <gamarcucci@gmail.com>
2023-03-13 21:45:14 -0300

    Desenvolvendo um jogo

---
+++ Estudo/Jogo/jogo.js
@@ -0,0 +1,15 @@
+
+var altura = 0
+var largura = 0 
+
+function ajustaTamanhoTela() {
+     altura = window.innerHeight
+     largura = window.innerWidth 
+     
+     console.log(altura, largura)
+     
+}
+
+
+
+
