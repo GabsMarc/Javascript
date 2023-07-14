a054624eec83962231151ba07ad1d7a92f1c3e3e
Gabriel Marcucci <gamarcucci@gmail.com>
2023-07-14 20:31:54 -0300

    commit

---
+++ Estudo/Curso Guanabara/Curso JS/Aula 6/script.js
@@ -0,0 +1,12 @@
+
+
+// confirm('Deseja fazer uma soma?', j) 
+// var n1 = prompt('Informe um numero')
+// var n2 = prompt('Informe um segundo numero')
+
+var soma = Number(n1) + Number(n2)
+// alert(`A soma dos dois números resultou em... ${soma} `)
+
+function minhafuncao() {
+    let text = 'Pressione o botão'    
+}
